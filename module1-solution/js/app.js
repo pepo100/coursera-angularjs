5d4b05d150fc9084a801cbc36329b3766605b383
pepo100 <peter.brudnak@gmail.com>
2017-10-21 00:38:50 +0200

    module1-solution added

---
+++ module1-solution/js/app.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    'use strict';
+
+    angular.module("LunchCheck", [])
+        .controller("LunchCheckController", LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+
+        $scope.items = "";
+        $scope.message = "";
+        $scope.messageClass = "message";
+
+        $scope.calculateSize = function () {
+            var itemsCount = 0;
+
+            angular.forEach($scope.items.split(","), function (item) {
+                if (item.trim() !== "")
+                    itemsCount++;
+            });
+
+            if (itemsCount !== 0) {
+                $scope.messageClass = "messageGreen";
+
+                
+                if (itemsCount <= 3)
+                    $scope.message = "Enjoy";
+                else
+                    $scope.message = "Too much!";
+            } else {
+                $scope.message = "Please enter data first";
+                $scope.messageClass = "messageRed";
+            }
+        };
+           
+    }
+})();
